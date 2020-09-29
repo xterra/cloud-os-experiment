857c985d6e30c150a3949ab7021f47d58ccf18da
Leonid Fedotov <3584432+iLeonidze@users.noreply.github.com>
2020-09-29 17:41:07 +0300

    Test fix

--- agent/xpointer.cpp
+++ agent/xpointer.cpp
@@ -8,18 +8,34 @@
 #include <X11/Xutil.h>
 
 void testPoints() {
-    std::cout << "Testing X";
+    std::cout << "Testing X" << std::endl;
     Display *display = XOpenDisplay(NULL);
     if(display == NULL){
         std::cerr << "Failed to detect main display!" << std::endl;
         exit(EXIT_FAILURE);
     }
-    XWarpPointer(display, None, None, 0, 0, 0, 0, 5, 5);
+
+    int screenNumber = DefaultScreen(display);
+    std::cerr << "Default screen: " << screenNumber << std::endl;
+    Window rootWin = DefaultRootWindow(display);
+    
+    XSelectInput(display, rootWin, KeyReleaseMask);
+    
+    std::cout << "5:5" << std::endl;
+    XWarpPointer(display, None, rootWin, 0, 0, 0, 0, 100, 100);
+    XFlush(display);
     sleep(1);
-    XWarpPointer(display, None, None, 0, 0, 0, 0, 50, 5);
+    std::cout << "50:5" << std::endl;
+    XWarpPointer(display, None, rootWin, 0, 0, 0, 0, 200, 200);
+    XFlush(display);
     sleep(1);
-    XWarpPointer(display, None, None, 0, 0, 0, 0, 5, 50);
+    std::cout << "5:50" << std::endl;
+    XWarpPointer(display, rootWin, rootWin, 0, 0, 0, 0, 5, 50);
+    XFlush(display);
     sleep(1);
-    XWarpPointer(display, None, None, 0, 0, 0, 0, 50, 50);
+    std::cout << "50:50" << std::endl;
+    XWarpPointer(display, rootWin, rootWin, 0, 0, 0, 0, 50, 50);
+    XFlush(display);
+    sleep(1);
     XCloseDisplay(display);
 }
