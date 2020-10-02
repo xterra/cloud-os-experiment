ea3a49188ea44da1bb5b45cc94f452ce4af24ef0
Leonid Fedotov <3584432+iLeonidze@users.noreply.github.com>
2020-10-02 17:52:25 +0300

    Remote controls added

--- agent/main.cpp
+++ agent/main.cpp
@@ -5,6 +5,8 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
+#include <X11/extensions/XTest.h>
+
 typedef websocketpp::server<websocketpp::config::asio> server;
 
 using websocketpp::lib::placeholders::_1;
@@ -17,6 +19,53 @@ typedef server::message_ptr message_ptr;
 Display *display;
 Window rootWin;
 
+
+enum remoteControlType {
+    UNKNOWN_CONTROL,
+    MOUSE,
+    TOUCH,
+    KEYBOARD
+};
+
+enum remoteControlEventType {
+    RESET,
+    MOUSEUP,
+    MOUSEDOWN,
+    MOUSEMOVE,
+    TOUCHSTART,
+    TOUCHEND,
+    TOUCHCANCEL,
+    TOUCHMOVE,
+    KEYDOWN,
+    KEYUP
+};
+
+std::vector<remoteControlEventType> eventTypeAssociation = {
+    RESET,
+    MOUSEUP,
+    MOUSEDOWN,
+    MOUSEMOVE,
+    TOUCHSTART,
+    TOUCHEND,
+    TOUCHCANCEL,
+    TOUCHMOVE,
+    KEYDOWN,
+    KEYUP
+};
+
+std::map<remoteControlEventType, remoteControlType> eventControlAssociation = {
+    { RESET, UNKNOWN_CONTROL },
+    { MOUSEUP, MOUSE },
+    { MOUSEDOWN, MOUSE },
+    { MOUSEMOVE, MOUSE },
+    { TOUCHSTART, TOUCH },
+    { TOUCHEND, TOUCH },
+    { TOUCHCANCEL, TOUCH },
+    { TOUCHMOVE, TOUCH },
+    { KEYDOWN, KEYBOARD },
+    { KEYUP, KEYBOARD }
+};
+
 std::vector<std::string> split(std::string str,std::string sep){
     char* cstr=const_cast<char*>(str.c_str());
     char* current;
@@ -28,6 +77,101 @@ std::vector<std::string> split(std::string str,std::string sep){
     }
     return arr;
 }
+
+class RemoteControlEvent {
+    public:
+        RemoteControlEvent(std::string payload) {
+            setRawData(payload);
+            std::vector<std::string> parsedRawData = split(payload, ":");
+            setEventType(parsedRawData[0]);
+            setTimestamp(parsedRawData[1]);
+            setValue(parsedRawData);
+        }
+        bool isValid() {
+            return valid;
+        }
+        std::string getRawData() {
+            return rawData;
+        }
+        int getTimestamp() {
+            return timestamp;
+        }
+        int getControlValues() {
+            return timestamp;
+        }
+        remoteControlEventType getEventType() {
+            return eventType;
+        }
+        remoteControlType getControlType() {
+            return controlType;
+        }
+        std::vector<std::string> getValue() {
+            return value;
+        }
+    private:
+        bool valid = true;
+        std::string rawData;
+        int timestamp = 0;
+        std::vector<std::string> value;
+        remoteControlType controlType = UNKNOWN_CONTROL;
+        remoteControlEventType eventType = RESET;
+        void setRawData(std::string data) {
+            rawData = data;
+        }
+        void setEventType(std::string typeNumber) {
+            try {
+                setEventType(std::stoi(typeNumber));
+            } catch (...) {
+                valid = false;
+            }
+        }
+        void setEventType(int typeNumber) {
+            try {
+                eventType = eventTypeAssociation[typeNumber];
+                setControlType(eventType);
+            } catch (...) {
+                valid = false;
+            }
+        }
+        void setControlType(remoteControlEventType eventType) {
+            try {
+                controlType = eventControlAssociation[eventType];
+            } catch (...) {
+                valid = false;
+            }
+        }
+        void setTimestamp(std::string timestamp) {
+            try {
+                setTimestamp(std::stoi(timestamp));
+            } catch (...) {
+                valid = false;
+            }
+        }
+        void setTimestamp(int timestampValue) {
+            timestamp = timestampValue;
+        }
+        void setValue(std::vector<std::string> data) {
+            value.clear();
+            for (int i = 0; i < data.size(); i++) {
+                if (i > 1) {
+                    value.push_back(data[i]);
+                }
+            }
+        }
+};
+
+
+int convertKey(std::string key) {
+    if (key.size() > 1) {
+        char *key_cstr = &key[0];
+        KeySym keysym = XStringToKeysym(key_cstr);
+        return XKeysymToKeycode(display, keysym);
+        return 0;
+    } else {
+        return XKeysymToKeycode(display, key.at(0));
+    }
+}
+
 
 // Define a callback to handle incoming messages
 void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
@@ -42,13 +186,58 @@ void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
 
     try {
         
-        std::string message = msg->get_payload();
-        std::vector<std::string> parsedMessage = split(message, ":");
+        RemoteControlEvent event = RemoteControlEvent(msg->get_payload());
+        std::vector<std::string> eventValue = event.getValue();
+        remoteControlEventType eventType = event.getEventType();
         
-        int x = std::stoi(parsedMessage[0]);
-        int y = std::stoi(parsedMessage[1]);
+        // XSendEvent(dis, PointerWindow,True,NoEventMask,e);
+        // XAllowEvents(dis, ReplayPointer, CurrentTime);
+        // XSync(dis, 0);
         
-        XWarpPointer(display, None, rootWin, 0, 0, 0, 0, x, y);
+        if (eventType == MOUSEMOVE || eventType == TOUCHMOVE) {
+            XWarpPointer(display, None, rootWin, 0, 0, 0, 0, std::stoi(eventValue[0]), std::stoi(eventValue[1]));
+        } else if (eventType == MOUSEDOWN || eventType == TOUCHSTART) {
+            XTestFakeButtonEvent(display, 1, True, CurrentTime);
+//            XTestFakeButtonEvent(display, 1, False, CurrentTime);
+//            XEvent ev;
+//            ev.xbutton.type = ButtonPress;
+//            ev.xbutton.type = ButtonRelease;
+//            ev.xkey.window = rootWin;
+//            ev.xkey.root = ev.xkey.subwindow = None;
+//            ev.xbutton.x = std::stoi(eventValue[0]);
+//            ev.xbutton.y = std::stoi(eventValue[1]);
+//            ev.xbutton.x_root = 0;
+//            ev.xbutton.y_root = 0;
+//            XSendEvent(display, rootWin, True, ButtonPress, &ev);
+        } else if (eventType == MOUSEUP || eventType == TOUCHEND || eventType == TOUCHCANCEL) {
+            XTestFakeButtonEvent(display, 1, False, CurrentTime);
+        } else if (eventType == KEYDOWN) {
+            int key = convertKey(eventValue[0]);
+            std::cout << "Key " << key << std::endl;
+            if (key != 0) {
+                XTestFakeKeyEvent(display, key, True, CurrentTime);
+            }
+//            XTestFakeKeyEvent(display, std::stoi(eventValue[0]), True, CurrentTime);
+//            XTestFakeKeyEvent(display, XKeysymToKeycode(display, eventValue[0].at(0)), True, CurrentTime);
+//            XEvent ev;
+//            KeySym key;
+//            ev.xkey.type = KeyPress;
+//            ev.xkey.window = rootWin;
+//            ev.xkey.root = ev.xkey.subwindow = None;
+//            ev.xkey.time = 0;
+//            ev.xkey.x = ev.xkey.y = ev.xkey.x_root = ev.xkey.y_root = 0;
+//            ev.xkey.state = 0;
+//            key = XStringToKeysym("7");
+//            ev.xkey.keycode = XKeysymToKeycode(display,key);
+//            ev.xkey.same_screen = True;
+//            XSendEvent(display, rootWin, True, KeyPressMask, &ev);
+        } else if (eventType == KEYUP) {
+            int key = convertKey(eventValue[0]);
+            if (key != 0) {
+                XTestFakeKeyEvent(display, key, False, CurrentTime);
+            }
+        }
+        
         XFlush(display);
         
         s->send(hdl, msg->get_payload(), msg->get_opcode());
@@ -101,4 +290,5 @@ int main() {
     } catch (...) {
         std::cout << "other exception" << std::endl;
     }
+    
 }
